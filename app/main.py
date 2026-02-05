7c090d7c797c3f33153c8aabdd02946e965b9ff9
arielhalifa <arielhalifa@gmail.com>
2026-02-05 11:34:26 +0200

    endpints ready

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+import dal
 import uvicorn
 
 
@@ -7,4 +8,34 @@ app = FastAPI()
 
 @app.get("/employees/engineering/high-salary")
 def high_salary():
-    pass
+    result = dal.get_engineering_high_salary_employees()
+    return result  
+
+@app.get("/employees/by-age-and-role")
+def by_age_and_role():
+    result = dal.get_employees_by_age_and_role()
+    return result
+
+@app.get("/employees/top-seniority")
+def top_seniority():
+    result = dal.get_top_seniority_employees_excluding_hr()
+    return result
+
+
+@app.get("/employees/age-or-seniority")
+def age_or_seniority():
+    result = dal.get_employees_by_age_or_seniority()
+    return result
+
+
+
+@app.get("/employees/managers/excluding-departments")
+def excluding_departments():
+    result = dal.get_managers_excluding_departments()
+    return result
+
+
+@app.get("/employees/by-lastname-and-age")
+def by_lastname_and_age():
+    result = dal.get_employees_by_lastname_and_age()
+    return result
