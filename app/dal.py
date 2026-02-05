1b3bc3508750729b50da6a739e2a8a95a015b80b
arielhalifa <arielhalifa@gmail.com>
2026-02-05 10:54:05 +0200

    mongo q4

--- app/dal.py
+++ app/dal.py
@@ -19,7 +19,7 @@ def get_employees_by_age_and_role():
     client = get_client()
     db = client['week-16-db']
     collection = db['employee_data_advanced']
-    by_age = collection.find({'age': {'$gte': 30, '$lte': 45}, 'job_role.department': {'$in': ['Specialist', 'Engineer']}})
+    by_age = collection.find({'age': {'$gte': 30, '$lte': 45}, 'job_role.department': {'$in': ['Specialist', 'Engineer']}}, {'_id': 0})
     client.close()
     return by_age
 
@@ -28,9 +28,16 @@ def get_top_seniority_employees_excluding_hr():
     client = get_client()
     db = client['week-16-db']
     collection = db['employee_data_advanced']
-    employees_7 = collection.find({'job_role.department': {'$ne': 'HR'}}).sort('years_at_company', -1 ).limit(7)
+    employees_7 = collection.find({'job_role.department': {'$ne': 'HR'}}, {'_id': 0}).sort('years_at_company', -1 ).limit(7)
     client.close()
     return employees_7
 
-    def get_employees_by_age_or_seniority():
-        
+def get_employees_by_age_or_seniority():
+    client = get_client()
+    db = client['week-16-db']
+    collection = db['employee_data_advanced']
+    employees = collection.find({'$or': [{'age': {'$gt':50}}, {'years_at_company': {'$lt': 3}}]},{'employee_id': 1, 'name': 1, 'age': 1, 'years_at_company': 1, '_id': 0})
+    client.close()
+    return employees
+
+
